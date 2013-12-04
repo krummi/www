0341f5d623f1ac944e1e8a1de67a3e11abd20211
Hrafn Eiríksson <hrafne@gmail.com>
2013-12-04 10:20:15 +0000

    Created the tagging backend.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,20 @@
 class Blog < ActiveRecord::Base
+
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def self.tagged_with(name)
+  	Tag.find(name: name).blogs
+  end
+
+  def tag_list
+  	tags.map(&:name).join(", ")
+  end
+
+  def tag_list=(names)
+  	self.tags = names.split(",").map do |t|
+  		Tag.where(name: t.strip).first_or_create!
+  	end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131202094206) do
+ActiveRecord::Schema.define(version: 20131204094547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,22 @@ ActiveRecord::Schema.define(version: 20131202094206) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "is_published"
+  end
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "blog_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["blog_id"], name: "index_taggings_on_blog_id", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
