6822e5ce841f07d6ffb40b79fbcea0afeb0e2a75
Hrafn Eiríksson <hrafne@gmail.com>
2013-12-06 10:53:46 +0000

    Added temporary image hack. Made CSS fixes so the blogs look better.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,6 +4,5 @@ class StaticController < ApplicationController
 
   def p2p
   	@blogs = Blog.tagged_with('p2p').where(is_published: true).order("published_at DESC")
-  	#@blogs = Blog.where(is_published: true).order(:published_at)
   end
 end
--- public/README.md
+++ public/README.md
@@ -10,3 +10,5 @@ My personal webpage.
   * ☐ Add my weight graph.
   * ☐ Migrate my blog to here.
   * ☐ "December 4, 2013" -> "December 4th, 2013". 
+  * ☐ Images on the blog.
+  * ☐ RSS.
