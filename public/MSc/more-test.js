8facbb08dfe16e68873da8954f65b4b6aabb5815
Hrafn Eiríksson <hrafne@gmail.com>
2016-02-27 14:27:45 +0000

    Deleting irrelevant things.

--- public/MSc/more-test.js
+++
@@ -1,7 +0,0 @@
-var page = require('webpage').create();
-page.open('http://www.dhs.state.il.us/accessibility/tests/flash/video.html', function () {
-    window.setTimeout(function(){
-        page.render('video.png');
-        phantom.exit();
-    }, 20000);
-});
--- public/MSc/test-flash.js
+++
@@ -1,7 +0,0 @@
-var page = require('webpage').create();
-page.open('http://old.oz.com/p2p/index.html?src=http://cdn.oz.com/channel/oz/ruv/playlist.m3u8', function () {
-    window.setTimeout(function(){
-        page.render('video.png');
-        phantom.exit();
-    }, 20000);
-});
