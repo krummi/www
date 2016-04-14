0772509d9d61dfa15382d382a0d8f4dea662c811
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-04-14 20:47:14 +0000

    Update Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,22 +1,24 @@
-var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-var less = require('gulp-less');
+'use strict';
+
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+const less = require('gulp-less');
 
 gulp.task('less', function () {
-  console.log('Compiling LESS.');
+  console.log('compiling LESS');
   gulp.src('./public/css/**/*.less')
     .pipe(less())
     .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('nodemon', function () {
-  console.log('Running nodemon...');
+gulp.task('nodemon', ['less'], function () {
+  console.log('running nodemon...');
   nodemon({
     script: 'bin/server',
     ext: 'js, jade, html, less',
-    ignore: ['README.md', 'node_modules/**', '.DS_Store']
-  })
-  .on('change', ['less']);
+    ignore: ['README.md', 'node_modules/**', '.DS_Store'],
+    tasks: ['less']
+  });
 });
 
 gulp.task('default', ['nodemon', 'less']);
